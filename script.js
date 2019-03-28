b9e8fd7ce71d9d7531456d5a6fbfd0b1b2cbfe6c
Velasquez <velasqga@88e9fe5957f6.ant.amazon.com>
2019-03-28 13:11:49 -0700

    final

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("img").click(function() {
+        // debugger;
+        var temp = $(this).attr('data-alt-src');
+        $(this).attr('data-alt-src', $(this).attr('src'));
+        $(this).attr('src', temp);
+    })
+});
